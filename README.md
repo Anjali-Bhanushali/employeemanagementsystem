41d439ca02e90847ed238509af5ebf8554ef54b8
Anjali kumari <anjalisinghbhanushali@gmail.com>
2026-01-13 03:19:04 +0530

    Improve login form UI and add submit handler
    Enhanced the Login component with styled input fields and a submit button, added a form submit handler, and updated input requirements. Also removed global text color from index.css, fixed import in main.jsx, clarified Tailwind usage in README, and cleaned up tailwind.config.js formatting.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # employeemanagementsystem
 A proffesional employee management system
+
+if the tailwind is not working type in terminal (ctrl+c) then (npm run dev)
+then it will start working
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
 const Login = () => {
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log("hello guyss, form submitted")
+    }
+
+
     return (
      <div className="flex h-screen w-screen items-center justify-center">
-      <div className="border-2 border-red-600 ">
-         <form className="flex flex-col items-center justify-center">
-            <input type="email" placeholder="Enter your email" />
-            <input type="password" placeholder="Enter your password" />
+      <div className="border-2 rounded-xl border-red-600 p-20 border-emerald-600 ">
+         <form onSubmit={(e) => {
+            submitHandler(e)
+         }} className="flex flex-col items-center justify-center">
+            <input required className="text-white bg-transparent  outline-none border-2 border-emerald-600 rounded-full py-3 px-5 text-xl placeholder:text-grey-400" type="email" placeholder="Enter your email" />
+            <input required className="text-white bg-transparent  outline-none border-2 border-emerald-600 rounded-full mt-10 py-3 px-5 text-xl placeholder:text-grey-400" type="password" placeholder="Enter your password" />
+            <button className="text-white  outline-none  bg-emerald-600 rounded-full mt-5 py-3 px-5 text-xl placeholder:text-white"  >Log in</button>
          </form>
       </div>
     </div>
