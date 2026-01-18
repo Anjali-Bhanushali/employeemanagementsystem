afd5433e82941fdcefaace8fcced6c2564f497bf
Anjali kumari <anjalisinghbhanushali@gmail.com>
2026-01-18 16:59:16 +0530

    updated all task to show all the employees in the admin dashboard

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -1,33 +1,20 @@
 import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
 
 const AllTask = () => {
+
+    const authData = useContext(AuthContext)
+
     return(
        <div className="bg-[1c1c1c] p-5 mt-5 rounded h-48 overflow-auto ">
-            <div className="bg-emerald-600 mb-2 py-2 px-4 flex justify-between rounded">
-                <h2>Anjali</h2>
+            {authData?.employees?.map((employees)=>{
+                return <div className="bg-emerald-600 mb-2 py-2 px-4 flex justify-between rounded">
+                <h2>{employees.name}</h2>
                 <h3>Make a UI Design</h3>
                 <h5>Status</h5>
-            </div>
-            <div className="bg-emerald-600 mb-2 py-2 px-4 flex justify-between rounded">
-                <h2>Anjali</h2>
-                <h3>Make a UI Design</h3> 
-                <h5>Status</h5>
-            </div>
-            <div className="bg-emerald-600 mb-2 py-2 px-4 flex justify-between rounded">
-                <h2>Anjali</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-            <div className="bg-emerald-600 mb-2 py-2 px-4 flex justify-between rounded">
-                <h2>Anjali</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
-            <div className="bg-emerald-600 mb-2 py-2 px-4 flex justify-between rounded">
-                <h2>Anjali</h2>
-                <h3>Make a UI Design</h3>
-                <h5>Status</h5>
-            </div>
+                </div>
+            })}
        </div>
     )
 }
